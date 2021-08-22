184024082656c6553f40232b55b24d112ab10dc0
FrancisFlow <iamngigikaranja1@aol.com>
2021-08-22 21:36:09 +0300

    Created the head section component and added background and center aligned text

---
+++ src/app/headsection/headsection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-headsection',
+  templateUrl: './headsection.component.html',
+  styleUrls: ['./headsection.component.css']
+})
+export class HeadsectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
